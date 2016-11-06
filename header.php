1ef3ebbed4840fa915c59b58a54242558036f579
Katie Paxton-Fear <K.Paxton-Fear@edu.salford.ac.uk>
2016-11-06 23:52:32 +0000

    Simple MVC structure

---
+++ header.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Katie
+ * Date: 06/11/2016
+ * Time: 23:46
+ */
---
+++ src/Twitter/User.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Katie
+ * Date: 06/11/2016
+ * Time: 23:51
+ */
+class User
+{
+
+}
